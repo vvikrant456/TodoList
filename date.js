9a1e6c3fba33b1b2d59868d6fdda8891d8514e89
vikas9162 <vvikrant456@gmail.com>
2021-07-16 05:49:14 +0530

    initial commit

---
+++ date.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+module.exports.getDate = function (){
+let today = new Date();
+let options = {
+weekday: "long",
+day: "numeric",
+month: "long"
+};
+ return today.toLocaleDateString("en-US", options);
+
+}
+
+module.exports.getDay = function(){
+let today = new Date();
+let options = {
+weekday: "long",
+// day: "numeric",
+// month: "long"
+};
+return today.toLocaleDateString("en-US", options);
+
+}
+
+// console.log(module.exports);
